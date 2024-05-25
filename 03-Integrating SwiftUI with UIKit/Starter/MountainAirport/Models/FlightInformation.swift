7986e74bfd367c8459e6d022350d63942d6cc707
Bill Morefield <bill@billmorefield.com>
2024-05-25 13:13:24 -0400

    Added properties needed for this lesson.

--- 03-Integrating SwiftUI with UIKit/Starter/MountainAirport/Models/FlightInformation.swift
+++ 03-Integrating SwiftUI with UIKit/Starter/MountainAirport/Models/FlightInformation.swift
@@ -31,6 +31,7 @@
 /// THE SOFTWARE.
 
 import SwiftUI
+import MapKit
 
 enum FlightDirection {
   case none
@@ -129,6 +130,26 @@ class FlightInformation: NSObject {
     timeFormatter.dateStyle = .none
     timeFormatter.timeStyle = .short
     return timeFormatter.string(from: time)
+  }
+  
+  var localAirportLocation: CLLocationCoordinate2D {
+    CLLocationCoordinate2D(latitude: 35.655, longitude: -83.4411)
+  }
+  
+  var startingAirportLocation: CLLocationCoordinate2D {
+    if direction == .arrival {
+      return CLLocationCoordinate2D(latitude: otherCoordinates.lat, longitude: otherCoordinates.long)
+    } else {
+      return localAirportLocation
+    }
+  }
+
+  var endingAirportLocation: CLLocationCoordinate2D {
+    if direction == .arrival {
+      return localAirportLocation
+    } else {
+      return CLLocationCoordinate2D(latitude: otherCoordinates.lat, longitude: otherCoordinates.long)
+    }
   }
 
   var flightStatus: String {
